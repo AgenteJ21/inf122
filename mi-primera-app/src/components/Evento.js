106ca587ddcc74feeee5983acf0b9ede08472918
AgenteJ21 <130728014+AgenteJ21@users.noreply.github.com>
2024-01-12 16:07:13 +0000

    intro react

---
+++ mi-primera-app/src/components/Evento.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../styles/Evento.css';
+function Evento() {
+    return (
+        <div className="card-container">
+        <img className="card-image"
+        src={require("../imagenes/pyPizza.webp")} 
+        alt="Foto del evento"/>
+        <div className="card-content">
+            <div className="card-title">
+                <h2>Py Pizza 03 - PyPuzzle</h2>
+            </div>
+            <p className="card-date">09 de Diciembre de 2023 - 13:30 GMT-4</p>
+            <p className="card-place">Mr Pizza, 20 de Octubre Frente a la Plaza Abaroa, La Paz, Bolivia</p>
+            <p className="card-description">¡Hola Pythonista!
+                Participa en este Evento Presencial de la comunidad Python La Paz
+                ¿Cuánto conoces de Python?
+                Aprende a trabajar en equipo resolviendo increíbles acertijo
+                ¡Anímate a ser parte de esta actividad!</p>
+            <div className="card-icons">
+                <img className="icons" 
+                src={require("../icons/facebook.svg")}
+                alt="" />
+                <img className="icons" 
+                src={require("../icons/youtube.svg")}
+                alt="" />
+                <img className="icons"
+                src={require("../icons/twitter.svg")}
+                alt="" /> 
+            </div>    
+        </div>
+    </div>
+    );
+}
+
+export default Evento;
---
+++ mi-primera-app/src/components/EventoB.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../styles/Evento.css';
+
+function EventoB(props) {
+    return (
+    <div className="card-container">
+        <img className="card-image"
+            src={require('´../imagenes/${props.image}.webp´)}
+            alt="Foto del evento" />
+        <div className="card-content">
+            <div className="card-title">
+                <h2>{props.title}</h2>
+            </div>
+            <p className="card-date">{props.dateLiteral}</p>
+            <p className="card-place">{props.placeLiteral}</p>
+            <p className="card-description">{props.description}</p>    
+        </div>
+    </div>
+    );
+}
+
+export default EventoB;
